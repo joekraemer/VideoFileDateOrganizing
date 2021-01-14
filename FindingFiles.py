82a4ee5acabc0d3554dfcae83ed43aeac520e9d2
joekraemer9 <joek@dmcinfo.com>
2021-01-13 20:46:23 -0800

    Add basic heat map graphing similar to the github activity heatmap.

--- FindingFiles.py
+++ FindingFiles.py
@@ -7,6 +7,10 @@ import shutil
 import pickle
 import datetime
 import time
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from collections import defaultdict
 from win32com.propsys import propsys, pscon
 from datetime import time as dt_time
@@ -15,11 +19,14 @@ from pathlib import Path
 current_dir_path = os.path.dirname(os.path.realpath(__file__))
 videoExtensions = ["*.mp4", "*.mov", "*.MP4", "*.avi", "*.mkv", "*.m4v"]
 photoExtensions = ["*.jpg", "*.heic", "*.ARW", "*.png", "*.dng", "*.jpeg"]
+DAYS = ['Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thurs.', 'Fri.', 'Sat.']
+MONTHS = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June',
+          'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.']
 pickleExtensions = ["*.pickle"]
 Destructive = False
 targetDirectory = 'E:/SSD\Media/OneSecond/Organized'
-sourceDirectory = 'F:\Content\2019'
-dictionaryFilename = 'dictionary.pickle'
+sourceDirectory = None
+dictionaryFilename = 'OneSecondDictionary.pickle'
 
 
 class FileInformation:
@@ -59,8 +66,8 @@ class FileClusterManager:
         # Name of folder when the FCM is instructed to put the files into a folder
         self.ClusterFolderName = str(self.Date.year)
 
-    # TODO: Prevent double adding files. Instead should maybe update the 
-    # last known location. 
+    # TODO: Prevent double adding files. Instead should maybe update the
+    # last known location.
     # Add files to this cluster
     def Add(self, file):
         # See if this file already exists
@@ -74,7 +81,7 @@ class FileClusterManager:
                     f.LastDirectoryFound = file.LastDirectoryFound
                     # Don't add this file and just return instead
                     return
-        # file doesn't exist in this FCM, add it to the list 
+        # file doesn't exist in this FCM, add it to the list
         self.Files.append(file)
         self.FilesExistOnDisk = True
         self.Size = self.Size + file.Size
@@ -209,6 +216,90 @@ def MakeFoldersForMonths(dir, earliestDate, latestDate):
         year = year + 1
 
 
+def date_heatmap(series, start=None, end=None, mean=False, ax=None, **kwargs):
+    '''Plot a calendar heatmap given a datetime series.
+
+    Arguments:
+        series (pd.Series):
+            A series of numeric values with a datetime index. Values occurring
+            on the same day are combined by sum.
+        start (Any):
+            The first day to be considered in the plot. The value can be
+            anything accepted by :func:`pandas.to_datetime`. The default is the
+            earliest date in the data.
+        end (Any):
+            The last day to be considered in the plot. The value can be
+            anything accepted by :func:`pandas.to_datetime`. The default is the
+            latest date in the data.
+        mean (bool):
+            Combine values occurring on the same day by mean instead of sum.
+        ax (matplotlib.Axes or None):
+            The axes on which to draw the heatmap. The default is the current
+            axes in the :module:`~matplotlib.pyplot` API.
+        **kwargs:
+            Forwarded to :meth:`~matplotlib.Axes.pcolormesh` for drawing the
+            heatmap.
+
+    Returns:
+        matplotlib.collections.Axes:
+            The axes on which the heatmap was drawn. This is set as the current
+            axes in the `~matplotlib.pyplot` API.
+    '''
+    # Combine values occurring on the same day.
+    dates = series.index.floor('D')
+    group = series.groupby(dates)
+    series = group.mean() if mean else group.sum()
+
+    # Parse start/end, defaulting to the min/max of the index.
+    start = pd.to_datetime(start or series.index.min())
+    end = pd.to_datetime(end or series.index.max())
+
+    # We use [start, end) as a half-open interval below.
+    end += np.timedelta64(1, 'D')
+
+    # Get the previous/following Sunday to start/end.
+    # Pandas and numpy day-of-week conventions are Monday=0 and Sunday=6.
+    start_sun = start - np.timedelta64((start.dayofweek + 1) % 7, 'D')
+    end_sun = end + np.timedelta64(7 - end.dayofweek - 1, 'D')
+
+    # Create the heatmap and track ticks.
+    num_weeks = (end_sun - start_sun).days // 7
+    heatmap = np.zeros((7, num_weeks))
+    ticks = {}  # week number -> month name
+    for week in range(num_weeks):
+        for day in range(7):
+            date = start_sun + np.timedelta64(7 * week + day, 'D')
+            if date.day == 1:
+                ticks[week] = MONTHS[date.month - 1]
+            if date.dayofyear == 1:
+                ticks[week] += f'\n{date.year}'
+            if start <= date < end:
+                heatmap[day, week] = series.get(date, 0)
+
+    # Get the coordinates, offset by 0.5 to align the ticks.
+    y = np.arange(8) - 0.5
+    x = np.arange(num_weeks + 1) - 0.5
+
+    # Plot the heatmap. Prefer pcolormesh over imshow so that the figure can be
+    # vectorized when saved to a compatible format. We must invert the axis for
+    # pcolormesh, but not for imshow, so that it reads top-bottom, left-right.
+    ax = ax or plt.gca()
+    mesh = ax.pcolormesh(x, y, heatmap, **kwargs)
+    ax.invert_yaxis()
+
+    # Set the ticks.
+    ax.set_xticks(list(ticks.keys()))
+    ax.set_xticklabels(list(ticks.values()))
+    ax.set_yticks(np.arange(7))
+    ax.set_yticklabels(DAYS)
+
+    # Set the current image and axes in the pyplot API.
+    plt.sca(ax)
+    plt.sci(mesh)
+
+    return ax
+
+
 class FileManager:
 
     def __init__(self):
@@ -218,6 +309,10 @@ class FileManager:
         self.firstDate = datetime.date(2020, 6, 15)
         self.lastDate = datetime.date(1987, 6, 15)
         self.LastTimeUpdated = datetime.datetime(1987, 1, 1)
+
+        # Strictly used for graphing
+        self.GraphNumberOfFiles = []
+        self.GraphDates = []
 
     # Add file directory to one of the date keys
     # TODO prevent a double add
@@ -313,6 +408,62 @@ class FileManager:
                 month = month + 1
             yr = yr + 1
 
+    # Iterates through dates within range, defualts to first and last date of dictionary
+    def DoSomethingForEverydateInRange(self, callback, firstDt=None, lastDt=None):
+        # Handle parameter defaults
+        if firstDt == None:
+            firstDt = self.firstDate
+        if lastDt == None:
+            lastDt = self.lastDate
+
+        yr = firstDt.year
+
+        while(yr <= lastDt.year):
+
+            # determine if we are in one of bounding years, we can't exceede the bounds
+            if yr == firstDt.year:
+                month = firstDt.month
+            else:
+                month = 1
+
+            if yr == lastDt.year:
+                limitMonth = lastDt.month
+            else:
+                limitMonth = 12
+
+            while(month <= limitMonth):
+                num_days = calendar.monthrange(yr, month)[1]
+                for day in range(1, num_days+1):
+                    date = datetime.date(yr, month, day)
+                    # Give the callback the date
+                    callback(date)
+
+                month = month + 1
+            yr = yr + 1
+
+    # create a two lists for Pandas Series given a range
+    def CreatePandasSeries(self, startDt, endDt):
+        self.DoSomethingForEverydateInRange(
+            self.AppendPandasSeries, startDt, endDt)
+        data = pd.Series(self.GraphNumberOfFiles)
+        data.index = pd.date_range( startDt, endDt, freq='1D')
+        return data
+
+    def AppendPandasSeries(self, dt):
+        # Unfortunately I have to append this to a class global variable
+        # TODO Find a more elegant way to have this callback access function level data
+
+        if (self.ValueExists(dt)):
+            self.GraphNumberOfFiles.append(self.files_by_date[dt].Number())
+        else:
+            self.GraphNumberOfFiles.append(0)
+
+        self.GraphDates.append(dt)
+
+    def ResetPandasSeries(self):
+        self.GraphNumberOfFiles.clear()
+        self.GraphDates.clear()
+
     # Print the dates that don't have any files
     def PrintMissingDates(self):
         self.CompleteCalendarDictionary()
@@ -341,7 +492,6 @@ class FileManager:
             print(str(year) + '-' + str(month) + " has " + str(daysWithFiles) +
                   " days with files. Missing " + str(daysMissing) + " days")
             month = month + 1
-
         return
 
     def GetNumberOfDaysThatHaveFilesInAGivenYear(self, year):
@@ -567,6 +717,57 @@ class FileManager:
         dt = datetime.datetime.fromtimestamp(mTime)
         return dt
 
+    def date_heatmap_plot(self, firstDt=None, lastDt=None):
+        '''An example for `date_heatmap`.
+
+        Most of the sizes here are chosen arbitrarily to look nice with 1yr of
+        data. You may need to fiddle with the numbers to look right on other data.
+        '''
+        # Handle parameter defaults
+        if firstDt == None:
+            firstDt = self.firstDate
+        if lastDt == None:
+            lastDt = self.lastDate
+
+        # Major issue will be getting my information into this data structure
+        ### Create Pandas Series ###
+        self.ResetPandasSeries()
+        s1 = self.CreatePandasSeries(firstDt, lastDt)
+
+        # Get some data, a series of values with datetime index.
+        # array of length 365 with values from 0 - 5
+        # data = np.random.randint(5, size=365)
+        # data = pd.Series(data)
+        # data.index = pd.date_range(
+        #   start='2017-01-01', end='2017-12-31', freq='1D')
+
+        # Create the figure. For the aspect ratio, one year is 7 days by 53 weeks.
+        # We widen it further to account for the tick labels and color bar.
+        figsize = plt.figaspect(7 / 56)
+        fig = plt.figure(figsize=figsize)
+
+        # Plot the heatmap with a color bar.
+        ax = date_heatmap(s1, edgecolor='black')
+        plt.colorbar(ticks=range(5), pad=0.02)
+
+        # Use a discrete color map with 5 colors (the data ranges from 0 to 4).
+        # Extending the color limits by 0.5 aligns the ticks in the color bar.
+        cmap = mpl.cm.get_cmap('Blues', 5)
+        plt.set_cmap(cmap)
+        plt.clim(-0.5, 4.5)
+
+        # Force the cells to be square. If this is set, the size of the color bar
+        # may look weird compared to the size of the heatmap. That can be corrected
+        # by the aspect ratio of the figure or scale of the color bar.
+        ax.set_aspect('equal')
+
+        # Save to a file. For embedding in a LaTeX doc, consider the PDF backend.
+        # http://sbillaudelle.de/2015/02/23/seamlessly-embedding-matplotlib-output-into-latex.html
+        fig.savefig('heatmapFiles.pdf', bbox_inches='tight')
+
+        # The firgure must be explicitly closed if it was not shown.
+        plt.close(fig)
+
 
 def main():
 
@@ -577,26 +778,32 @@ def main():
 
     lstMod = ref.LastTimeModified(targetDirectory)
     lstUpdated = ref.LastTimeUpdated
-    # if(lstMod > lstUpdated):
-    # Add videos to dictionary that are already in the Target Directory
+    if(lstMod > lstUpdated):
+        # Add videos to dictionary that are already in the Target Directory
+        print('Target Directory Updated, rescanning...')
+        # ref.IndexVideosInDirectory(targetDirectory)
 
-    ref.IndexVideosInDirectory(targetDirectory)
+    if sourceDirectory != None:
+        if Destructive:
+            ref.DeletePhotosInDirectory(sourceDirectory)
 
-    if Destructive:
-        ref.DeletePhotosInDirectory(sourceDirectory)
+            # Add videos to dictionary and then copy them
+            ref.CutVideosFromDirectory(sourceDirectory, targetDirectory)
 
-        # Add videos to dictionary and then copy them
-        ref.CutVideosFromDirectory(sourceDirectory, targetDirectory)
-
-    else:
-        # Add videos to dictionary and then delete them
-        ref.CopyVideosFromDirectory(sourceDirectory, targetDirectory)
+        else:
+            # Add videos to dictionary and then delete them
+            ref.CopyVideosFromDirectory(sourceDirectory, targetDirectory)
 
     ref.PrintNumberOfMissingDatesPerMonth(2017)
     ref.PrintNumberOfMissingDatesPerMonth(2018)
     ref.PrintNumberOfMissingDatesPerMonth(2019)
     ref.PrintNumberOfMissingDatesPerMonth(2020)
     ref.PrintNumberOfMissingDatesPerYear()
+
+    start = datetime.date(2017, 1, 1)
+    end = datetime.date(2018, 12, 31)
+    ref.date_heatmap_plot(start, end)
+
     ref.SaveDictionary(targetDirectory, dictionaryFilename)
 
 
